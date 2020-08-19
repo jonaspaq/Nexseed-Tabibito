7d78b74a97fe569d38d7e7d1c5795e0a3d603fa3
SasanoHayato <sasano.hayato@gmail.com>
2020-08-19 09:20:18 +0800

    set

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 <html>
 
 <head>
+  
 	<!-- Google Tag Manager-->
   <script>
-  //   (function (w, d, s, l, i) {
-  //     w[l] = w[l] || []; w[l].push({
-  //       'gtm.start':
-  //         new Date().getTime(), event: 'gtm.js'
-  //     }); var f = d.getElementsByTagName(s)[0],
-  //       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-  //         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-  //   })(window, document, 'script', 'dataLayer', 'GTM-MMNTKMJ');
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MMNTKMJ');
   </script>
   <!-- End Google Tag Manager-->
 	<title>オンライン留学 Seed Tech Online夜間じっくりコースを開講！</title>
@@ -103,6 +104,7 @@
 
 <!-- <body class="is-preload"> -->
 <body class="">
+
 	<!-- Google Tag Manager (noscript)-->
   <noscript>
     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMNTKMJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>

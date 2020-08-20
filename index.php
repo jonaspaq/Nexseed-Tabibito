6f2660a9904a7fbc37b9f849a6c055c69059c38e
SasanoHayato <sasano.hayato@gmail.com>
2020-08-20 12:45:52 +0800

    set

--- index.php
+++ index.php
@@ -5,16 +5,13 @@
 <head>
   
 	<!-- Google Tag Manager-->
-  <script>
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || []; w[l].push({
-        'gtm.start':
-          new Date().getTime(), event: 'gtm.js'
-      }); var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-MMNTKMJ');
-  </script>
+ <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WDKPCBN');</script>
+<!-- End Google Tag Manager -->
   <!-- End Google Tag Manager-->
 	<title>オンライン留学 Seed Tech Online夜間じっくりコースを開講！</title>
 	<meta charset="utf-8" />
@@ -106,9 +103,14 @@
 <body class="">
 
 	<!-- Google Tag Manager (noscript)-->
-  <noscript>
+  <!-- <noscript>
     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMNTKMJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-  </noscript>
+  </noscript> -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDKPCBN"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
   <!-- End Google Tag Manager (noscript)-->
 	<!-- Google Tag Manager (noscript) -->
 	<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K95V8MS" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
